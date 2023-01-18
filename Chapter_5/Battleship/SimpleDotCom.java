d933b88322e1784714bbbe58da9c0d4fd5286020
Dan-BV <110034308+Dan-BV@users.noreply.github.com>
2023-01-18 22:34:33 +0300

    Added GameHelper and LaunchGame classes

--- Chapter_5/Battleship/SimpleDotCom.java
+++ Chapter_5/Battleship/SimpleDotCom.java
@@ -1,19 +1,3 @@
-
-
-class SimpleDotComTestDrive{
-    public static void main(String[] args) {
-        SimpleDotCom dot = new SimpleDotCom();
-
-        int[] locations = {2, 3, 4};
-
-        dot.setLocationsCells(locations);
-
-        String userGuess = "1";
-
-        String result = dot.checkYourself(userGuess);
-    }
-}
-
 public class SimpleDotCom{
 
     int numOfHits = 0;
